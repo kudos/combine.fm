9a5088251d83aba21283aa1d45dcf9d4fc64d73d
Jonathan Cremin <jonathan@crem.in>
2015-08-21 20:56:50 +0100

    More 404 work

--- views/notfound.js
+++ views/notfound.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Head from './head';
 import Foot from './foot';
 
@@ -9,13 +10,20 @@ export default React.createClass({
       <html>
       <Head {...this.props} />
       <body>
-      <div className='error vertical-center'>
+      <div className='error'>
         <div className='container main'>
           <div className='row'>
             <div className='col-md-12'>
+              <div className='error-logo'>
+                <Link to='home'><img src='/images/logo-full-300.png' width='50' /></Link>
+              </div>
+            </div>
+          </div>
+          <div className='row vertical-center'>
+            <div className='col-md-12'>
               <h2>404</h2>
               <h1>Sorry, it looks like the page you asked for is gone.</h1>
-              <a href='/'>Take Me Home</a> or <a href='https://www.youtube.com/watch?v=gnnIrTLlLyA' target='_blank'>Show Me the Wubs</a>
+              <Link to='home'>Take Me Home</Link> or <a href='https://www.youtube.com/watch?v=gnnIrTLlLyA' target='_blank'>Take Me to the Wubs</a>
             </div>
           </div>
         </div>
