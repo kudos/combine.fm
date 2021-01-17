dd5e3897392a9edea21a4a744bc115cd090c38c6
Renato "Lond" Cerqueira <renato@lond.com.br>
2021-01-17 12:05:16 +0100

    Add check for streamUrl

--- test/services/ytmusic.js
+++ test/services/ytmusic.js
@@ -37,10 +37,12 @@ describe('ytmusic', function(){
       it('should parse track url into ID', async function (){
         const result = await ytmusic.parseUrl('https://music.youtube.com/watch?v=YLp2cW7ICCU&feature=share');
         result.id.should.equal("YLp2cW7ICCU");
+        result.streamUrl.should.equal("https://music.youtube.com/watch?v=YLp2cW7ICCU");
       });
       it('should parse album url into ID', async function (){
         const result = await ytmusic.parseUrl('https://music.youtube.com/browse/MPREb_9C36yscfgmJ');
         result.id.should.equal("MPREb_9C36yscfgmJ");
+        result.streamUrl.should.equal("https://music.youtube.com/browse/MPREb_9C36yscfgmJ");
       });
       it('should parse alternative album url into ID', async function (){
         const result = await ytmusic.parseUrl('https://music.youtube.com/playlist?list=OLAK5uy_lx9K5RpiBEwd3E4C1GKqY7e06qTlwydvs');
