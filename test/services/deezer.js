3ea715de39b95b57ef0c35849bc05ea7fcf562e0
Jonathan Cremin <jonathan@crem.in>
2025-05-20 13:18:38 +0100

    Some service api fixes

--- test/services/deezer.js
+++ test/services/deezer.js
@@ -1,45 +1,62 @@
-import 'should';
-import * as deezer from '../../lib/services/deezer/index.js';
+import "should";
+import * as deezer from "../../lib/services/deezer/index.js";
 
-describe('Deezer', () => {
-  describe('lookupId', () => {
-    it('should find album by ID', async function test() {
-      const result = await deezer.lookupId('302127', 'album');
-      result.name.should.equal('Discovery');
+describe("Deezer", () => {
+  describe("lookupId", () => {
+    it("should find album by ID", async function test() {
+      const result = await deezer.lookupId("302127", "album");
+      result.name.should.equal("Discovery");
     });
 
-    it('should find track by ID', async function (){
-      const result = await deezer.lookupId('3135554', 'track');
-      result.name.should.equal('Aerodynamic');
-    });
-  });
-
-  describe('search', () => {
-    it('should find album by search', async function (){
-      const result = await deezer.search({type: 'album', artist: {name: 'Jamie xx'}, name: 'In Colour'});
-      result.name.should.startWith('In Colour');
-    });
-
-    it('should find album with various artists by search', async function (){
-      const result = await deezer.search({type: 'album', artist: {name: 'Various Artists'}, name: 'The Trevor Nelson Collection'});
-      result.name.should.equal('The Trevor Nelson Collection');
-    });
-
-    it('should find track by search', async function (){
-      const result = await deezer.search({type: 'track', artist: {name: 'Deftones'}, albumName: 'Deftones', name: 'Hexagram'});
-      result.name.should.equal('Hexagram');
+    it("should find track by ID", async function() {
+      const result = await deezer.lookupId("3135554", "track");
+      result.name.should.equal("Aerodynamic");
     });
   });
 
-  describe('lookupUrl', () => {
-    describe('parseUrl', () => {
-      it('should parse album url into ID', async function (){
-        const result = await deezer.parseUrl('http://www.deezer.com/album/302127');
+  describe("search", () => {
+    it("should find album by search", async function() {
+      const result = await deezer.search({
+        type: "album",
+        artist: { name: "Jamie xx" },
+        name: "In Colour"
+      });
+      result.name.should.startWith("In Colour");
+    });
+
+    it("should find album with various artists by search", async function() {
+      const result = await deezer.search({
+        type: "album",
+        artist: { name: "Various Artists" },
+        name: "Rocket League x Monstercat Vol. 6"
+      });
+      result.name.should.equal("Rocket League x Monstercat Vol. 6");
+    });
+
+    it("should find track by search", async function() {
+      const result = await deezer.search({
+        type: "track",
+        artist: { name: "Deftones" },
+        albumName: "Deftones",
+        name: "Hexagram"
+      });
+      result.name.should.equal("Hexagram");
+    });
+  });
+
+  describe("lookupUrl", () => {
+    describe("parseUrl", () => {
+      it("should parse album url into ID", async function() {
+        const result = await deezer.parseUrl(
+          "http://www.deezer.com/album/302127"
+        );
         result.id.should.equal(302127);
       });
 
-      it('should parse track url into ID', async function (){
-        const result = await deezer.parseUrl('http://www.deezer.com/track/3135554');
+      it("should parse track url into ID", async function() {
+        const result = await deezer.parseUrl(
+          "http://www.deezer.com/track/3135554"
+        );
         result.id.should.equal(3135554);
       });
     });
--- test/services/itunes.js
+++ test/services/itunes.js
@@ -1,46 +1,54 @@
-import 'should';
-import * as itunes from '../../lib/services/itunes/index.js';
+import "should";
+import * as itunes from "../../lib/services/itunes/index.js";
 
-describe('iTunes Music', function(){
-  describe('lookupId', function(){
-    it('should find album by ID', async function (){
-      const result = await itunes.lookupId('1445991287', 'album');
-      result.name.should.equal('Peace Orchestra');
+describe("iTunes Music", function() {
+  describe("lookupId", function() {
+    it("should find album by ID", async function() {
+      const result = await itunes.lookupId("1445991287", "album");
+      result.name.should.equal("Peace Orchestra");
     });
 
-    it('should find track by ID', async function (){
-      const result = await itunes.lookupId('1445927701', 'track');
-      result.name.should.equal('Double Drums');
-    });
-  });
-
-  describe('search', function(){
-    it('should find album by search', async function (){
-      const result = await itunes.search({type: 'album', artist: {name: 'Deftones'}, name: 'White Pony'});
-      result.name.should.equal('White Pony');
-    });
-
-    it('should find awkward album by search', async function (){
-      const result = await itunes.search({type: 'album', artist: {name: 'Anavitória'}, name: 'Fica'});
-      result.name.should.equal('Fica (feat. Matheus & Kauan) - Single');
-    });
-
-    it('should find track by search', async function (){
-      const result = await itunes.search({type: 'track', artist: {name: 'Deftones'}, albumName: 'Deftones', name: 'Hexagram'});
-      result.name.should.equal('Hexagram');
+    it("should find track by ID", async function() {
+      const result = await itunes.lookupId("1440718994", "track");
+      result.name.should.equal("Zombie");
     });
   });
 
-  describe('lookupUrl', function(){
-    describe('parseUrl', function(){
-      it('should parse album url into ID', async function (){
-        const result = await itunes.parseUrl('https://itunes.apple.com/us/album/peace-orchestra/1445991287');
-        result.id.should.equal('us1445991287');
+  describe("search", function() {
+    it("should find album by search", async function() {
+      const result = await itunes.search({
+        type: "album",
+        artist: { name: "Deftones" },
+        name: "White Pony"
+      });
+      result.name.should.equal("White Pony");
+    });
+
+    it("should find track by search", async function() {
+      const result = await itunes.search({
+        type: "track",
+        artist: { name: "Deftones" },
+        albumName: "Deftones",
+        name: "Hexagram"
+      });
+      result.name.should.equal("Hexagram");
+    });
+  });
+
+  describe("lookupUrl", function() {
+    describe("parseUrl", function() {
+      it("should parse album url into ID", async function() {
+        const result = await itunes.parseUrl(
+          "https://itunes.apple.com/us/album/peace-orchestra/1445991287"
+        );
+        result.id.should.equal("us1445991287");
       });
 
-      it('should parse track url into ID', async function (){
-        const result = await itunes.parseUrl('https://itunes.apple.com/us/album/double-drums-dj-dsl-mix/1445927689?i=1445927701');
-        result.id.should.equal('us1445927689');
+      it("should parse track url into ID", async function() {
+        const result = await itunes.parseUrl(
+          "https://itunes.apple.com/us/album/double-drums-dj-dsl-mix/1445927689?i=1445927701"
+        );
+        result.id.should.equal("us1445927689");
       });
     });
   });
