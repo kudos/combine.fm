cf942fa6ca93bccbef8ad1063003dd1caf8a37df
Jonathan Cremin <jonathan@crem.in>
2026-04-21 19:40:38 +0100

    One final cleanup

--- public/src/entry-client.js
+++
@@ -1,9 +0,0 @@
-// import { style } from 'bulma/css/bulma.css';
-import { sync } from 'vuex-router-sync';
-import { app, store, router } from './app';
-
-document.addEventListener('DOMContentLoaded', () => { // eslint-disable-line no-undef
-  store.replaceState(window.__INITIAL_STATE__); // eslint-disable-line
-  sync(store, router);
-  app.$mount('#app');
-});
